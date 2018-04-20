ef205ecdbb71f22091e1254a5defdec808052aeb
C5-DEV-IRUFUS\Ishamel Rufus <ishmael.rufus@cloud5.com>
2018-04-20 15:15:19 -0500

    Scripts completed

---
+++ jshellbot-install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ -z ${JSBOT_INSTALL+x}]; then
+  echo "JSBOT_INSTALL needs to be set as an environment variable"
+  exit 1
+fi
+
+BASE_DIR=$JSBOT_INSTALL
+
+gradle clean
+if gradle fatJar; then
+  #move all jar files to the new location
+  pattern="./build/libs/org.togetherjava.*.jar"
+  files=( $pattern )
+  echo "${files[0]}"
+  cp ${files[0]} $JSBOT_INSTALL/jshellbot.jar
+  #copy jshellbot.sh and create service
+  cp ./jshellbot.sh /etc/init.d/
+  echo "Service installed. To start type `service jshellbot start`"
+else
+  echo "Failed to install service"
+fi
