90e07bcc58fc89f9c8e149e52bc950e6725ea95f
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2018-04-26 22:32:40 +0200

    Add renderers for different return objects
    * Added a dedicated renderer for Exceptions, Compilation errors,
      Standard output and Strings. The latter two now also limit the amount
      of newlines they display, to reduce the size of the messages.
    
    * Added some tests for the truncating behaviour

---
+++ src/main/java/org/togetherjava/discord/server/rendering/RenderUtils.java
@@ -0,0 +1,54 @@
+package org.togetherjava.discord.server.rendering;
+
+import sx.blah.discord.util.EmbedBuilder;
+
+import java.awt.Color;
+
+class RenderUtils {
+
+    static int NEWLINE_MAXIMUM = 10;
+
+    private static final Color ERROR_COLOR = new Color(255, 99, 71);
+    private static final Color SUCCESS_COLOR = new Color(118, 255, 0);
+
+    /**
+     * Truncates the String to the max length and sanitizes it a bit.
+     *
+     * @param input     the input string
+     * @param maxLength the maximum length it can have
+     * @return the processed string
+     */
+    static String truncateAndSanitize(String input, int maxLength) {
+        StringBuilder result = new StringBuilder();
+
+        int newLineCount = 0;
+        for (int codePoint : input.codePoints().toArray()) {
+            if (codePoint == '\n') {
+                newLineCount++;
+            }
+
+            if (codePoint == '\n' && newLineCount > NEWLINE_MAXIMUM) {
+                result.append("⏎");
+            } else {
+                result.append(Character.toChars(codePoint));
+            }
+        }
+
+        return truncate(result.toString(), maxLength);
+    }
+
+    private static String truncate(String input, int maxLength) {
+        if (input.length() <= maxLength) {
+            return input;
+        }
+        return input.substring(0, maxLength);
+    }
+
+    static void applyFailColor(EmbedBuilder builder) {
+        builder.withColor(ERROR_COLOR);
+    }
+
+    static void applySuccessColor(EmbedBuilder builder) {
+        builder.withColor(SUCCESS_COLOR);
+    }
+}
