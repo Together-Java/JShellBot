d61ba8a1e808c4a602b813d489d24849e24a3993
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2018-09-15 15:28:56 +0200

    Fix D4J test code remaining in a test

--- src/test/java/org/togetherjava/discord/server/rendering/TruncationRendererTest.java
+++ src/test/java/org/togetherjava/discord/server/rendering/TruncationRendererTest.java
@@ -1,10 +1,10 @@
 package org.togetherjava.discord.server.rendering;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import net.dv8tion.jda.core.entities.MessageEmbed;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
-import sx.blah.discord.util.EmbedBuilder;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TruncationRendererTest {
 
@@ -12,7 +12,7 @@ class TruncationRendererTest {
     void truncatesNewLines() {
         String string = StringUtils.repeat("\n", 40) + "some stuff";
 
-        String rendered = RenderUtils.truncateAndSanitize(string, EmbedBuilder.FIELD_CONTENT_LIMIT);
+      String rendered = RenderUtils.truncateAndSanitize(string, MessageEmbed.VALUE_MAX_LENGTH);
 
         int newLines = StringUtils.countMatches(rendered, '\n');
         assertEquals(RenderUtils.NEWLINE_MAXIMUM, newLines, "Expected 10 newlines");
@@ -22,7 +22,7 @@ class TruncationRendererTest {
     void keepsNewlines() {
         String string = StringUtils.repeat("\n", RenderUtils.NEWLINE_MAXIMUM) + "some stuff";
 
-        String rendered = RenderUtils.truncateAndSanitize(string, EmbedBuilder.FIELD_CONTENT_LIMIT);
+      String rendered = RenderUtils.truncateAndSanitize(string, MessageEmbed.VALUE_MAX_LENGTH);
 
         int newLines = StringUtils.countMatches(rendered, '\n');
         assertEquals(RenderUtils.NEWLINE_MAXIMUM, newLines, "Expected 10 newlines.");
